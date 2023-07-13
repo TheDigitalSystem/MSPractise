b42804cfeddedca4f6124fe2e5881a570923bf51
TheDigitalSystem <macidzadeh@gmail.com>
2023-07-13 23:53:34 +0400

    Kanan commit

---
+++ src/BMW.java
@@ -0,0 +1,5 @@
+public class BMW {
+    public static void main(String[] args) {
+        System.out.println("salam");
+    }
+}
---
+++ src/Mercedes.java
@@ -0,0 +1,5 @@
+public class Mercedes {
+    public static void main(String[] args) {
+        System.out.println("salamda");
+    }
+}
